efcbb41699c453303f42c3792046a65cebfbce2c
Jonathan Prieto <jonathan.prieto@me.com>
2019-09-23 07:54:35 -0500

    Ok

---
+++ cmake-build-debug/CMakeFiles/binary-search-tree-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binary-search-tree-jonathanprieto99.dir/main.cpp.o"
+  "CMakeFiles/binary-search-tree-jonathanprieto99.dir/tester/tester.cpp.o"
+  "CMakeFiles/binary-search-tree-jonathanprieto99.dir/mocker/mocker.cpp.o"
+  "binary-search-tree-jonathanprieto99.pdb"
+  "binary-search-tree-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binary-search-tree-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
